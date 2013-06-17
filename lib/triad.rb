214212df58d300d4d3196a58462b1ec491617a24
Jim Gay <jim@saturnflyer.com>
2013-06-17 19:10:15 -0400

    move to hash storage

--- lib/triad.rb
+++ lib/triad.rb
@@ -4,27 +4,28 @@ class Triad
   include Enumerable
 
   def initialize(*args)
-    @keys = []
-    @descriptors = []
-    @values = []
+    @storage = {}
   end
 
   attr_reader :keys, :descriptors, :values
   private :keys, :descriptors, :values
 
   def key(string_or_object)
-    collection = string_or_object.is_a?(String) ? descriptors : values
-    keys[collection.index(string_or_object)]
+    Array(@storage.find{|key, array|
+      array[0] == string_or_object || array[1] == string_or_object
+    }).first
   end
 
   def descriptor(sym_or_object)
-    collection = sym_or_object.is_a?(Symbol) ? keys : values
-    descriptors[collection.index(sym_or_object)]
+    Array(Array(@storage.find{|key, array|
+      array[0] == string_or_object || array[1] == string_or_object
+    })[1])[0]
   end
 
   def value(symbol_or_string)
-    collection = symbol_or_string.is_a?(Symbol) ? keys : descriptors
-    values[collection.index(symbol_or_string)]
+    Array(Array(@storage.find{|key, array|
+      array[0] == string_or_object || array[1] == string_or_object
+    })[1])[1]
   end
 
   def <<(array)
@@ -32,9 +33,7 @@ class Triad
     array_key = array.find{|item| item.is_a?(Symbol) }
     array_descriptor = array.find{|item| item.is_a?(String) }
     array_value = array.find{|item| !item.is_a?(String) && !item.is_a?(Symbol) }
-    keys << array_key
-    descriptors << array_descriptor
-    values << array_value
+    @storage[array_key] = [array_descriptor, array_value]
     self
   end
 
